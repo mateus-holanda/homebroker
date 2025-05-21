c6425cece8de09094f9d2c173beda43a7578acf3
Mateus Holanda <mateus23ita@gmail.com>
2025-05-21 20:21:16 -0300

    feat: connect an asset to a wallet

--- api/src/wallets/entities/wallet.entity.ts
+++ api/src/wallets/entities/wallet.entity.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { randomUUID } from 'crypto';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { WalletAsset, WalletAssetDocument } from './wallet-asset.entity';
 
 export type WalletDocument = HydratedDocument<Wallet>;
 
@@ -8,6 +9,13 @@ export type WalletDocument = HydratedDocument<Wallet>;
 export class Wallet {
   @Prop({ default: () => randomUUID() })
   _id: string;
+
+  @Prop({
+    type: [mongoose.Schema.Types.String],
+    set: (v) => [...new Set(v)],
+    ref: WalletAsset.name,
+  })
+  assets: WalletAssetDocument[] | string[];
 
   createdAt!: Date;
 
--- api/src/wallets/wallets.service.ts
+++ api/src/wallets/wallets.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { WalletAsset } from './entities/wallet-asset.entity';
 import { Wallet } from './entities/wallet.entity';
@@ -11,6 +11,7 @@ export class WalletsService {
     @InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
     @InjectModel(WalletAsset.name)
     private walletAssetSchema: Model<WalletAsset>,
+    @InjectConnection() private connection: mongoose.Connection,
   ) {}
 
   create(data: CreateWalletDto) {
@@ -22,18 +23,49 @@ export class WalletsService {
   }
 
   findOne(id: string) {
-    return this.walletSchema.findById(id);
+    return this.walletSchema
+      .findById(id)
+      .populate([{ path: 'assets', populate: ['asset'] }]);
   }
 
-  createWalletAsset(data: {
+  async createWalletAsset(data: {
     walletId: string;
     assetId: string;
     shares: number;
   }) {
-    return this.walletAssetSchema.create({
-      wallet: data.walletId,
-      asset: data.assetId,
-      shares: data.shares,
-    });
+    const session = await this.connection.startSession();
+
+    session.startTransaction();
+
+    try {
+      const docs = await this.walletAssetSchema.create(
+        [
+          {
+            wallet: data.walletId,
+            asset: data.assetId,
+            shares: data.shares,
+          },
+        ],
+        { session },
+      );
+
+      const walletAsset = docs[0];
+
+      await this.walletSchema.updateOne(
+        { _id: data.walletId },
+        { $push: { assets: walletAsset._id } },
+        { session },
+      );
+
+      await session.commitTransaction();
+
+      return walletAsset;
+    } catch (e) {
+      console.error(e);
+      await session.abortTransaction();
+      throw e;
+    } finally {
+      await session.endSession();
+    }
   }
 }
